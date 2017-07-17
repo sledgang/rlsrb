2bfbfa120d2817fc3eeb85140062de37b7ff8fa9
Zac Nowicki <zachnowicki@gmail.com>
2017-07-17 22:38:15 +0000

    document Utils

--- lib/rls/utils.rb
+++ lib/rls/utils.rb
@@ -3,9 +3,14 @@
 require 'time'
 
 module RLS
+  # A helper module of various utility methods
   module Utils
     module_function
 
+    # Parses a unix epoch time into a `Time` object.
+    #
+    # @param ts [Integer] epoch time
+    # @return [Time]
     def time(ts)
       Time.at(ts)
     end
