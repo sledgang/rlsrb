7897021e739673854466e752824b91bd19691762
snapcase <snapcase@octalzeroes.com>
2017-07-18 00:21:53 +0200

    add messages to exceptions

--- lib/rls/error.rb
+++ lib/rls/error.rb
@@ -1,6 +1,15 @@
 module RLS
   module Error
-    class MissingKey < RuntimeError; end
-    class InvalidKey < RuntimeError; end
+    class MissingKey < RuntimeError
+      def message
+        'You need to supply a valid API key'
+      end
+    end
+
+    class InvalidKey < RuntimeError
+      def message
+        'Invalid API key'
+      end
+    end
   end
 end
