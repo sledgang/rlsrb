afe1fde16761d5500691114b18aae18cb1534f83
snapcase <snapcase@octalzeroes.com>
2017-07-18 00:11:05 +0200

    moved require statements

--- lib/rls/client.rb
+++ lib/rls/client.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+require 'rls/error'
+require 'rls/api'
+require 'rls/utils'
+
 module RLS
   # A Client that abstracts a user's API key from REST requests,
   # and provides other helpful higher-level functionality.
